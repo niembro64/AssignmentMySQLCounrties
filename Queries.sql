a30505a4aed3b1e45535d94fa418bfbde7a3b13c
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-16 19:28:54 -0600

    4 Done

--- Queries.sql
+++ Queries.sql
@@ -20,6 +20,11 @@ WHERE countries.name = "Mexico" AND cities.population > 500000
 ORDER BY cities.population DESC;
 
 -- 4. What query would you run to get all languages in each country with a percentage greater than 89%? Your query should arrange the result by percentage in descending order. (1)
+SELECT country.name, languages.language, languages.percentage
+FROM languages
+LEFT JOIN country ON country.code = languages.country_code
+WHERE percentage > 89
+ORDER BY percentage DESC;
 
 -- 5. What query would you run to get all the countries with Surface Area below 501 and Population greater than 100,000? (2)
 
